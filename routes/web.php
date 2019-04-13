2e84ed54c0ad9a429662cba8e756e169b697b801
Thibaultdb <debackerthibault@gmail.com>
2019-04-13 19:23:42 +0100

    Data on client

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/about', 'PagesController@about');
 
 Route::get('/contact', 'PagesController@contact');
 
+Route::get('/projects', 'ProjectsController@index');
 
 // Route::get('/about', function () {
 //     return view('about');
